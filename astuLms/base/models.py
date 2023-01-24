17afe74b19d72734eb73041f89dca24ea0309bf3
kaleabbyh <kaleabbyh@gmail.com>
2023-01-24 10:32:36 +0300

    backend updated

--- astuLms/base/models.py
+++ astuLms/base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField('student status', default=False)
+    is_teacher = models.BooleanField('teacher status', default=False)
